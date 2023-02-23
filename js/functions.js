8bf8a6ca4418510a8e88585a01362f394ac3e76d
Pronin Maks <maksimpronin1991@icloud.com>
2023-02-23 20:28:30 +0300

    finish homework

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-const checkStringLength = function (string,maxLength) {
-  if(string.length <= maxLength){
-    return true;
+const checkStringLength = (string,maxLength) => string.length <= maxLength;
+
+// eslint-disable-next-line no-unused-vars
+const checkPalindrome = (string) => {
+  const lowerString = string.toLowerCase().replaceAll(' ','');
+  let reversedString = '';
+  for(let i = lowerString.length - 1 ; i >= 0 ; i--){
+    reversedString += lowerString[i];
   }
-  return false;
+  return lowerString === reversedString;
 };
 
+
+// eslint-disable-next-line no-unused-vars
+const returnNumber = (string) => {
+  let result = '';
+  for(let i = 0; i < string.length; i ++){
+    if(!Number.isNaN(parseInt(string[i],10))){
+      result += string[i];
+    }
+  }
+  return parseInt(result,10);
+};
+
+const padStrig = (string,minLength,addition) => {
+  const freePlace = minLength - string.length;
+
+  if(freePlace <= 0){
+    return string;
+  }
+  return addition.slice(0 , freePlace % addition.length) + addition.repeat(freePlace / addition.length) + string;
+};
