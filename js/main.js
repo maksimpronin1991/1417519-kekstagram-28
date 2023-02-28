be1c6de51e3c11d4992a9f54cb5419973044148e
Pronin Maks <maksimpronin1991@icloud.com>
2023-02-28 22:48:37 +0300

    complited  exercise module4

--- js/main.js
+++ js/main.js
@@ -1,47 +1,85 @@
-/*
-В файле main.js напишите необходимые функции для создания массива
-из 25 сгенерированных объектов. Каждый объект массива — описание фотографии,
-опубликованной пользователем.
+const DESCRIPTIONS = [
+  'good picture',
+  'nice view',
+  'look amazing',
+  'holywood smile',
+  'nice chuse',
+  'beautiful',
+  'great'
+];
 
-Структура каждого объекта должна быть следующей:
+const MESSAGES_FOR_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.' ,
+  'В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.' ,
+  'Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = [
+  'Артём',
+  'Вася',
+  'Паша',
+  'Петя',
+  'Вика',
+  'Настя',
+  'Юля'
+];
 
-id, число — идентификатор опубликованной фотографии.
- Это число от 1 до 25. Идентификаторы не должны повторяться.
 
-url, строка — адрес картинки вида photos/{{i}}.jpg,
- где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться.
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
 
-description, строка — описание фотографии.
- Описание придумайте самостоятельно.
+  return Math.floor(result);
+};
 
-likes, число — количество лайков, поставленных фотографии.
-Случайное число от 15 до 200.
+const createRandomNumberFromRangeGenerator = (min, max) => {
+  const previousValues = [];
 
-comments, массив объектов — список комментариев,
- оставленных другими пользователями к этой фотографии.
- Количество комментариев к каждой фотографии вы определяете на своё усмотрение.
-  Все комментарии генерируются случайным образом. Пример описания объекта с комментарием:
-
-{
-  id: 135,
-  avatar: 'img/avatar-6.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: 'Артём',
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
 }
-У каждого комментария есть идентификатор — id — любое число. Идентификаторы не должны повторяться.
 
-Поле avatar — это строка, значение которой формируется по правилу
-img/avatar-{{случайное число от 1 до 6}}.svg. Аватарки подготовлены в директории img.
+const createRandomId = createRandomNumberFromRangeGenerator(1,25);
+const createRandomUrlNumber = createRandomNumberFromRangeGenerator(1,25);
+const createRandomIdForComment = createRandomNumberFromRangeGenerator(1,10000);
+const createRandomAvatarNumber = createRandomNumberFromRangeGenerator(1,6);
+const arrPicturesDescriptions = [];
 
-Для формирования текста комментария — message — вам необходимо взять одно или два случайных
-предложения из представленных ниже:
+const createPictureDescription = () => ({
+  id: createRandomId(),
+  url: `photos/${createRandomUrlNumber()}.jpg`,
+  description: DESCRIPTIONS[getRandomInteger(0,DESCRIPTIONS.length - 1)],
+  likes: getRandomInteger(15,200),
+  comments: [
+    {
+      id: createRandomIdForComment(),
+      avatar: `img/avatar-${createRandomAvatarNumber()}.svg`,
+      message: MESSAGES_FOR_COMMENTS[getRandomInteger(0,MESSAGES_FOR_COMMENTS.length - 1)],
+      name: NAMES[getRandomInteger(0,NAMES.length - 1)],
+    },
+    {
+      id: createRandomIdForComment(),
+      avatar: `img/avatar-${createRandomAvatarNumber()}.svg`,
+      message: MESSAGES_FOR_COMMENTS[getRandomInteger(0,MESSAGES_FOR_COMMENTS.length - 1)],
+      name: NAMES[getRandomInteger(0,NAMES.length - 1)],
+    }
+  ]
+});
 
-Всё отлично!
-В целом всё неплохо. Но не всё.
-Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.
-Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.
-Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.
-Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!
-Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами. Подставляйте
- случайное имя в поле name.
- */
+for(let i = 0; i < 25; i ++){
+  arrPicturesDescriptions.push(createPictureDescription())
+};
