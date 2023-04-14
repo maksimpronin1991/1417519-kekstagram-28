98b83789098da069ad92e977203e985285fe4e80
Pronin Maks <maksimpronin1991@icloud.com>
2023-04-14 20:14:44 +0300

    message for upload status remove when click outside that

--- js/message.js
+++ js/message.js
@@ -1,6 +1,6 @@
 import { isEscapeKey } from './util.js';
-const successMessage = document.querySelector('#success').content.querySelector('.success');
-const errorMessage = document.querySelector('#error').content.querySelector('.error');
+const successMessage = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
+const errorMessage = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
 const closeBtnSucces = successMessage.querySelector('.success__button');
 const closeBtnError = errorMessage.querySelector('.error__button');
 
@@ -12,27 +12,40 @@ const onDocumentKeydown = (evt) => {
 };
 
 const onOutsideMessage = (evt) => {
+  const click = evt.composedPath().includes(document.querySelector('.success__inner'));
+  if(!click){
+    document.querySelector('.upload__status').remove();
+    document.body.classList.remove('modal-open');
+  }
 };
 
 function showSuccessMessage () {
   document.body.append(successMessage);
+  document.body.classList.add('modal-open');
+
   document.addEventListener('keydown', onDocumentKeydown);
   document.addEventListener('click', onOutsideMessage);
 }
 
 function showErrorMessage () {
   document.body.append(errorMessage);
+  document.body.classList.add('modal-open');
+
   document.addEventListener('keydown', onDocumentKeydown);
   document.addEventListener('click', onOutsideMessage);
 }
 
 function closeErrorMessage (){
   errorMessage.remove();
+  document.body.classList.remove('modal-open');
+
   document.removeEventListener('keydown', onDocumentKeydown);
   document.removeEventListener('click', onOutsideMessage);
 }
 function closeSuccsesMessage (){
   successMessage.remove();
+  document.body.classList.remove('modal-open');
+
   document.removeEventListener('keydown', onDocumentKeydown);
   document.removeEventListener('click', onOutsideMessage);
 }
